2db69fc8abcb268b8c3227422c26ef5c499127f8
Sebastian Pedroza <sebaspedroza.developer@gmail.com>
2022-11-22 15:00:51 -0500

    :construction: SEO with Helmet

--- src/components/MetaHead/MetaHead.jsx
+++ src/components/MetaHead/MetaHead.jsx
@@ -16,18 +16,18 @@ export function MetaHead({ title, description, image, url }) {
       <link rel="canonical" href={url} />
       <meta property="og:locale" content="en_US" />
       <meta property="og:type" content="article" />
-      <meta property="og:title" content={`MocaFood - ${title}`} />
+      <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
-      <meta property="og:site_name" content="MocaFood" />
+      <meta property="og:site_name" content="SebasDeveloper" />
       <meta property="og:image" content={image} />
       <meta property="og:image:secure_url" content={image} />
       <meta property="og:image:width" content="1280" />
       <meta property="og:image:height" content="720" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@davecast_" />
-      <meta name="twitter:creator" content="@davecast_" />
-      <meta name="twitter:title" content={`MocaFood ${title}`} />
+      <meta name="twitter:site" content="@SebasDeveloper" />
+      <meta name="twitter:creator" content="@SebasDeveloper" />
+      <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
     </Helmet>
