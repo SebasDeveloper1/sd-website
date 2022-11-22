3a2374d1fd6ed3db459543e7253fa1572dbaf967
Sebastian Pedroza <sebaspedroza.developer@gmail.com>
2022-11-21 22:28:59 -0500

    :construction: SEO with Helmet

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import {
   HeroHome,
   AboutSection,
@@ -12,15 +13,29 @@ import './Home.scss';
 
 export function Home() {
   return (
-    <main className="home-page">
-      <div id="top" />
-      <HeroHome />
-      <AboutSection />
-      <AbilitiesSection />
-      <ProjectsSection />
-      <PreparationSection />
-      <ContactSection />
-      <GoToUpButton />
-    </main>
+    <>
+      <Helmet>
+        <title>SebasDeveloper | Home</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@SebasDeveloper" />
+        <meta name="twitter:creator" content="@SebasDeveloper" />
+        <meta name="twitter:title" content="SebasDeveloper" />
+        <meta name="twitter:description" content="Portafolio SebasDeveloper" />
+        <meta
+          name="twitter:image"
+          content="https://firebasestorage.googleapis.com/v0/b/sd-website-f934d.appspot.com/o/sebasDeveloper%2Fabout-img.jpg?alt=media&token=eb68dc54-90d6-4d99-8052-19d7f6b7476c"
+        />
+      </Helmet>
+      <main className="home-page">
+        <div id="top" />
+        <HeroHome />
+        <AboutSection />
+        <AbilitiesSection />
+        <ProjectsSection />
+        <PreparationSection />
+        <ContactSection />
+        <GoToUpButton />
+      </main>
+    </>
   );
 }
